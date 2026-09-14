5bbcd4496c17bec61d6014e889ea3de22063a55a
Mattia-Carcione <mattia.carcione01@gmail.com>
2026-09-14 11:52:36 +0200

    fix: fixato oggettto network

--- src/infrastructure/context/accessor.context.ts
+++ src/infrastructure/context/accessor.context.ts
@@ -29,6 +29,7 @@ export class RequestContextAccessor
           req: '',
           res: '',
         },
+        csrf: '',
       },
       tracing: {
         correlationId: GuidHelper.generate(),
